699fa33a4cb51c0baefe4e42ddd8de88324e29c5
MattheusKasa <mattheus.kasa@hotmail.se>
2023-01-06 03:22:59 +0000

    Add PostDetail view to retrieve, edit and delete posts

--- profiles/views.py
+++ profiles/views.py
@@ -10,7 +10,8 @@ from hobbyz_api.permissions import IsOwnerOrReadOnly
 class ProfileList(APIView):
     def get(self, request):
         profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True, context={'request': request})
+        serializer = ProfileSerializer(
+            profiles, many=True, context={'request': request})
         return Response(serializer.data)
 
 
@@ -33,7 +34,8 @@ class ProfileDetail(APIView):
 
     def put(self, request, pk):
         profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, data=request.data, context={'request': request})
+        serializer = ProfileSerializer(
+            profile, data=request.data, context={'request': request})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
