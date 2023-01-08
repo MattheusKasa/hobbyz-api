cfb301527fe501a52b521b9045b15b99c5f2a08c
MattheusKasa <mattheus.kasa@hotmail.se>
2023-01-08 17:12:02 +0000

    Add LikeList and LikeDetail generic view, view and delete likes

--- likes/serializers.py
+++ likes/serializers.py
@@ -1,6 +1,6 @@
 from django.db import IntegrityError
 from rest_framework import serializers
-from likes.model import Like
+from likes.models import Like
 
 
 class LikeSerializer(serializers.ModelSerializer):
@@ -14,7 +14,7 @@ class LikeSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         try:
             return super().create(validated_data)
-            except IntergrityError:
-                raise serializers.ValidationError({
-                    'detail': 'possible duplicate'
-                })
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
