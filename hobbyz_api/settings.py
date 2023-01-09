fa7c59d62b59fda063f83d72d1fe898fa5408190
MattheusKasa <mattheus.kasa@hotmail.se>
2023-01-09 04:37:11 +0000

    Add ALLOWED_HOST from environ

--- hobbyz_api/settings.py
+++ hobbyz_api/settings.py
@@ -59,7 +59,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'hobbyz-api.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'), 'localhost', 'hobbyz-api.herokuapp.com']
 
 
 # Application definition
